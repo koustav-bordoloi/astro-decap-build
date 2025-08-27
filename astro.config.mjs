f321af841256ed7dbbae2909e1bff9672812328b
Koustav Bordoloi <koustavbordoloi@gmail.com>
2025-08-27 14:30:53 +0530

    netlify server adapter added

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,15 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import decapCmsOauth from 'astro-decap-cms-oauth';
 
+import netlify from '@astrojs/netlify/functions';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  integrations: [decapCmsOauth()]
 
+  integrations: [decapCmsOauth()],
+  output: 'server', // required for serverless routes
+  adapter: netlify({})
 });
